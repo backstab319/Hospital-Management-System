dedcabd9654677d593e56d91ca13e1dfd4c297af
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-02 19:28:14 +0530

    Update manage_medicine.php

--- manage/manage_medicine.php
+++ manage/manage_medicine.php
@@ -26,6 +26,10 @@
         </navbar>
     </div>
 
+    <?php
+        include "../connect.php";
+    ?>
+
     <div class="container jumbotron text-center my-4 col-lg-6 col-xl-6">
         <h1 class="display-4">Manage Medicine's Data</h1>
         <div class="container">
@@ -33,6 +37,159 @@
             <a class="btn btn-outline-primary" href="/manage/secondary/medicine_secondary.php">Medicine Additional Info</a>
         </div>
     </div>
+
+    <div class="container text-center my-4 col-lg-6 col-xl-6">
+        <h1 class="display-4">View Medicine's Info</h1>
+        <div class="container text-center col-lg-7 col-xl-7">
+            <?php
+                showmeds();
+                function showmeds(){
+                    global $conn;
+                    $sql = "SELECT * FROM medicine_info";
+                    $result = $conn->query($sql);
+                    if($result->num_rows > 0){
+                        show($result);
+                    }else{
+                        echo "<p class='lead'>There are no medicine's</p>";
+                    }
+                }
+                function show($result){
+                    echo '<table class="table table-bordered table-striped table-hover">
+                    <thead class="thead-primary">
+                    <tr>
+                    <th>Medicine name</th>
+                    <th>Quantity</th>
+                    <th>MG</th>
+                    <th>Description</th>
+                    <th>Price</th>
+                    </tr>
+                    </thead>';
+                    while($row = $result->fetch_assoc()){
+                        
+                        echo "<tr><td>".$row['name']."</td><td>".$row['quantity']."</td><td>".$row['mg']."</td><td>".$row["description"]."</td><td>".$row["price"]."</td></tr>";
+                    }
+                    echo "</table>";
+                }
+            ?>
+        </div>
+    </div>
+
+    <div class="container text-center my-4 col-lg-6 col-xl-6">
+        <h1 class="display-4">Add Medicine's</h1>
+        <div class="container text-center col-lg-7 col-xl-7">
+            <form method="POST" action="manage_medicine.php">
+                <div class="form-group">
+                    <input type="text" class="form-control my-2" name="mname" placeholder="Medicine's name">
+                    <input type="number" class="form-control my-2" name="quantity" placeholder="Quantity">
+                    <input type="number" class="form-control my-2" name="mg" placeholder="MG">
+                    <input type="text" class="form-control my-2" name="des" placeholder="Description">
+                    <input type="number" class="form-control my-2" name="price" placeholder="Price">
+                    <input type="submit" class="form-control my-2 btn btn-outline-primary" value="Add Doctor" name="add">
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="container text-center my-4 col-lg-6 col-xl-6">
+        <h1 class="display-4">Update Medicine's Information</h1>
+        <p class="text-justify lead">Please select the information that you want to update with the information that you want it to change it to.</p>
+        <div class="container text-center col-lg-7 col-xl-7">
+            <form method="POST" action="manage_medicine.php">
+                <div class="form-group">
+                    <input type="text" class="form-control my-2" name="oldval" placeholder="Enter old vlaue">
+                    <label for="column">Select Information to change</label>
+                    <select class="form-control my-2" name="column" id="column">
+                        <option value="name">Medicine name</option>
+                        <option value="quantity">Quantity</option>
+                        <option value="mg">MG</option>
+                        <option value="description">Description</option>
+                        <option value="price">Price</option>
+                    </select>
+                    <input type="text" class="form-control my-2" name="newval" placeholder="Enter new vlaue">
+                    <input type="submit" class="form-control my-2" value="Update" name="update">
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <div class="container text-center my-4 col-lg-6 col-xl-6">
+        <h1 class="display-4">Delete Medicine's Information</h1>
+        <div class="container text-center col-lg-7 col-xl-7">
+            <form method="POST" action="manage_medicine.php">
+                <div class="form-group">
+                    <form method="POST" action="manage_medicine.php">
+                        <label for="del_val">Select medicine's name to delete</label>
+                        <select class="form-control my-2" name="delval" id="del_val">
+                            <?php
+                                dispoption();
+                                function dispoption(){
+                                    global $conn;
+                                    $sql = "SELECT name FROM medicine_info";
+                                    $result = $conn->query($sql);
+                                    if($result->num_rows > 0){
+                                        while($row = $result->fetch_assoc()){
+                                            echo "<option value=".$row["name"].">".$row["name"]."</option>";
+                                        }
+                                    }
+                                }
+                            ?>
+                        </select>
+                        <input type="submit" class="form-control my-2" value="Delete" name="delete">
+                    </form>
+                </div>
+            </form>
+        </div>
+    </div>
+
+    <?php
+        if(isset($_POST["add"])){
+            add_med();
+        }
+        if(isset($_POST["update"])){
+            update_med();
+        }
+        if(isset($_POST["delete"])){
+            delete();
+        }
+        function add_med(){
+            global $conn;
+            $mname = $_POST["mname"];
+            $quantity = $_POST["quantity"];
+            $mg = $_POST["mg"];
+            $des= $_POST["description"];
+            $price = $_POST["price"];
+            if(($mname and $quantity and $mg and $des and $price) != NULL){
+                $sql = "INSERT INTO medicine_info VALUES ('$mname',$quantity,$mg,'$des',$price)";
+                $conn->query($sql);
+            }
+        }
+        function update_med(){
+            global $conn;
+            $oldval = $_POST["oldval"];
+            $column = $_POST["column"];
+            $newval = $_POST["newval"];
+            $sql = "UPDATE medicine_info SET $column='$newval' WHERE $column='$oldval'";
+            $conn->query($sql);
+        }
+        function delete(){
+            global $conn;
+            if(isset($_POST["delval"])){
+                $val = $_POST["delval"];
+                $sql = "DELETE FROM medicine_info WHERE name='$val'";
+                $conn->query($sql);
+                $sql = "SELECT * FROM medicine_secondary WHERE name='$val'";
+                $result = $conn->query($sql);
+                if($result->num_rows > 0){
+                    deleteadd($val);
+                }
+            }
+            function deleteadd($val){
+                global $conn;
+                $sql = "DELETE FROM medicine_secondary WHERE name='$val'";
+                $conn->query($sql);
+            }
+        }
+    ?>
     
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
