2ceca044cb8197038eb72ad071f6e80406b5a638
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-21 01:17:37 +0530

    Update manage_data.php

--- manage_data.php
+++ manage_data.php
@@ -76,6 +76,22 @@
             <a href="/manage/manage_tests.php" class="btn btn-outline-primary">Manage Test's</a>
         </div>
     </div>
+
+    <div class="container text-center my-4 col-lg-6 col-xl-6">
+        <h1 class="display-4">Blood Donation</h1>
+        <div class="container text-center">
+            <p class="lead text-justify text-center">Manage blood donation data</p>
+            <a href="/manage/manage_blooddonation.php" class="btn btn-outline-primary">Blood Donation Data</a>
+        </div>
+    </div>
+
+    <div class="container text-center my-4 col-lg-6 col-xl-6">
+        <h1 class="display-4">Blood Emergency Request</h1>
+        <div class="container text-center">
+            <p class="lead text-justify text-center">Manage blood emergency request</p>
+            <a href="/manage/manage_bloodrequest.php" class="btn btn-outline-primary">Blood Donation Data</a>
+        </div>
+    </div>
     
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
