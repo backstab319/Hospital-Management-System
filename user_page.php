8858f22bf8404c6581dfc747f0f7bfb02c57ab09
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-21 01:17:42 +0530

    Update user_page.php

--- user_page.php
+++ user_page.php
@@ -25,7 +25,23 @@
             </div>
         </navbar>
     </div>
-    
+
+    <div class="appointment d-flex align-items-center">
+        <div class="container text-center text-justify col-lg-6 col-xl-6">
+            <h1 class="display-4">Appointment</h1>
+            <p class="lead">Please go to the following link to take an appointment</p>
+            <a href="appointment.php" class="btn btn-outline-primary">Appointment</a>
+        </div>
+    </div>
+
+    <div class="bloodcamp d-flex align-items-center">
+        <div class="container text-center text-justify col-lg-6 col-xl-6">
+            <h1 class="display-4">Blood Bank</h1>
+            <p class="lead">You can also donate blood to save someones life. Please goto the following link to get and appointment to donate or request blood for emergency.</p>
+            <a href="blood_bank.php" class="btn btn-outline-light">Blood Bank</a>
+        </div>
+    </div>
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
