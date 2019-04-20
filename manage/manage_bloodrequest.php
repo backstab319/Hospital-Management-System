b536178981d1d304d2fe9e13f264b13c0a9cb198
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-21 01:17:33 +0530

    Create manage_bloodrequest.php

---
+++ manage/manage_bloodrequest.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="utf-8">
+    <title>Blood request</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="/style.css">
+</head>
+<body>
+    <div class="navbar-section home">
+        <navbar class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
+            <div class="container">
+                <a class="navbar-brand">HMS</a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#toggle"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="toggle">
+                    <div class="navbar-menu ml-auto">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a class="nav-link" href="">Home</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </navbar>
+    </div>
+
+    <?php
+        include "../connect.php";
+    ?>
+
+    <div class="container text-center my-4 col-lg-6 col-xl-6">
+        <h1 class="display-4">View Blood Requests Data</h1>
+        <div class="container text-center col-lg-7 col-xl-7">
+            <?php
+                showdonation();
+                function showdonation(){
+                    global $conn;
+                    $sql = "SELECT * FROM blood_request";
+                    $result = $conn->query($sql);
+                    if($result->num_rows > 0){
+                        show($result);
+                    }else{
+                        echo "<p class='lead'>There are no donee's</p>";
+                    }
+                }
+                function show($result){
+                    echo '<table class="table table-bordered table-striped table-hover">
+                    <thead class="thead-primary">
+                    <tr>
+                    <th>Donee Name</th>
+                    <th>Phone</th>
+                    <th>Blood Group</th>
+                    </tr>
+                    </thead>';
+                    while($row = $result->fetch_assoc()){
+                        
+                        echo "<tr><td>".$row['name']."</td><td>".$row['phone']."</td><td>".$row["blood_group"]."</td></tr>";
+                    }
+                    echo "</table>";
+                }
+            ?>
+        </div>
+    </div>
+
+    <div class="container text-center my-4 col-lg-6 col-xl-6">
+        <h1 class="display-4">Delete Blood Donee's Form</h1>
+        <div class="container text-center col-lg-7 col-xl-7">
+            <div class="form-group">
+                <form method="POST" action="manage_bloodrequest.php">
+                    <label for="del_val">Select donee's name to delete</label>
+                        <select class="form-control my-2" name="delval" id="del_val">
+                            <?php
+                                dispoption();
+                                function dispoption(){
+                                    global $conn;
+                                    $sql = "SELECT name FROM blood_request";
+                                    $result = $conn->query($sql);
+                                    if($result->num_rows > 0){
+                                        while($row = $result->fetch_assoc()){
+                                            echo "<option value='".$row["name"]."'>".$row["name"]."</option>";
+                                        }
+                                    }
+                                }
+                            ?>
+                        </select>
+                    <input type="submit" class="form-control my-2" value="Delete" name="delete">
+                </form>
+            </div>
+        </div>
+        <?php
+            if(isset($_POST["delete"])){
+                delete();
+            }
+            function delete(){
+                global $conn;
+                $delval = $_POST["delval"];
+                $sql = "DELETE FROM blood_request WHERE name = '$delval'";
+                $conn->query($sql);
+            }
+        ?>
+    </div>
+    
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
