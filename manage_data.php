500e7730b596954ae159463950fbe04cb3ba51a7
backstab319 <dev.siddhartha.gupta@gmail.com>
2019-04-21 13:04:03 +0530

    Update manage_data.php

--- manage_data.php
+++ manage_data.php
@@ -92,6 +92,14 @@
             <a href="/manage/manage_bloodrequest.php" class="btn btn-outline-primary">Blood Donation Data</a>
         </div>
     </div>
+
+    <div class="container text-center my-4 col-lg-6 col-xl-6">
+        <h1 class="display-4">Appointments</h1>
+        <div class="container text-center">
+            <p class="lead text-justify text-center">Manage Appointments</p>
+            <a href="/manage/manage_appointments.php" class="btn btn-outline-primary">Appointments</a>
+        </div>
+    </div>
     
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
